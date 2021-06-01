d38e1d666b78ed5880d88367e6acbbb57421888c
Dev01-vinamr-Ken <73330048+Dev01-vinamr-Ken@users.noreply.github.com>
2021-06-01 23:26:41 +0700

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,11 @@
 import GameSection from '../components';
 
-import Menu from '../components/Menu/Menu';
-import NewGameBar from '../components/Menu/NewGameBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <GameSection/> */}
-      <Menu/>
+      <GameSection/>
     </div>
   );
 }
