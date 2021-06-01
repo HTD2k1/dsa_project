8fe236ea6692b9b6f286f61cffccc489e578687f
baryenmunich123 <thanhphongth132@gmail.com>
2021-06-01 20:04:39 +0700

    fix newgamebar

--- src/app/App.js
+++ src/app/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <GameSection/>
+      {/* <GameSection/> */}
       <Menu/>
     </div>
   );
--- src/components/Menu/NewGameBar.js
+++ src/components/Menu/NewGameBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './NewGameBar.css';
 
 function NewGame(props) {
     return (
