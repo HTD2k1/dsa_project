68b08f9aee5b4a2992d5ebe8934e7ed8c0dfcf77
HTD2k1 <superdatlocpro@gmail.com>
2021-06-04 00:26:28 +0700

    Update StatusSection.js

--- src/components/Status/StatusSection.js
+++ src/components/Status/StatusSection.js
@@ -8,7 +8,20 @@ function StatusSection(props) {
   const sudokuTableData = props.sudokuTableData;
   return (
     <div className="status">
-      <MenuBar sudokuTableData={sudokuTableData} onSolveSudoku={solvedTable => props.onSolveSudoku(solvedTable)} />
+      <MenuBar
+      //Data
+      sudokuTableData={sudokuTableData} 
+      undoCellStack={props.undoCellStack}
+      redoCellStack={props.redoCellStack} 
+      //Handling game Functionality
+      onSolveSudoku={solvedTable => props.onSolveSudoku(solvedTable)}
+      onUndo ={stacks => props.onUndo(stacks) }
+      onRedo ={stacks => props.onRedo(stacks) }
+      //Handle change game Mode
+      onModeChange ={table => props.onModeChange(table)}
+
+      />
+
       <Keypad onClickNumber={(number) => props.onClickNumber(number)} />
     </div>
   );
