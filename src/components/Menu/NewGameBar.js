030b9dfd92bca595210914196c98da882eb6464f
HTD2k1 <superdatlocpro@gmail.com>
2021-06-04 00:26:26 +0700

    Update NewGameBar.js

--- src/components/Menu/NewGameBar.js
+++ src/components/Menu/NewGameBar.js
@@ -1,32 +1,36 @@
 import React from 'react';
+import { SUDOKU_DATA, MEDIUM_SUDOKU, HARD_SUDOKU, EXPERT_SUDOKU } from "../../config/constants/gameData";
+function NewGameBar(props) {
 
-function NewGame(props) {
+    const changeMode =(mode) =>{
+        switch(mode){
 
-    const handleEasy = () =>{
+            case 'EASY':
+                return SUDOKU_DATA
+                
+            case 'MEDIUM':
+                return MEDIUM_SUDOKU
+            
+            case 'HARD':
+                return HARD_SUDOKU
+            
+            case 'EXPERT':
+                return EXPERT_SUDOKU
+            default:
+                break;
+        }
 
-    }
-
-    const handleMedium = () =>{
-        
-    }
-
-    const handleHard = () =>{
-        
-    }
-
-    const handleExpert = () =>{
-        
     }
 
     return (
         <div className = "newGameBar">
             <h1 className = "header">Choose game's mode</h1>
-            <button className = "easy" onClick ={handleEasy}>Easy</button>
-            <button className = "medium" onClick ={handleMedium}>Medium</button>
-            <button className = "hard" onClick ={handleHard}>Hard</button>
-            <button className = "expert" onClick ={handleExpert}>Expert</button>
+            <button className = "easy" onClick ={() => props.onModeChange(changeMode('EASY'))}>Easy</button>
+            <button className = "medium" onClick ={() => props.onModeChange(changeMode('MEDIUM'))}>Medium</button>
+            <button className = "hard" onClick ={() => props.onModeChange(changeMode('HARD'))}>Hard</button>
+            <button className = "expert" onClick ={() => props.onModeChange(changeMode('EXPERT'))}>Expert</button>
         </div>
     );
 }
 
-export default NewGame;
+export default NewGameBar;
