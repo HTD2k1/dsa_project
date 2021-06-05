7b4d28388bbf931be5227e2a3fe04fbda8557f68
HTD2k1 <superdatlocpro@gmail.com>
2021-06-05 16:19:21 +0700

    Highlight after clicking cell

--- src/components/Game/GameTable.js
+++ src/components/Game/GameTable.js
@@ -3,10 +3,24 @@ import React, { useState } from "react";
 import "./GameTable.css";
 
 function GameTable(props) {
-  const [clickCell, setClickCell] = React.useState({
-    rowIndex: null,
-    colIndex: null,
-  });
+  // const [clickCell, setClickCell] = React.useState({
+  //   rowIndex: null,
+  //   colIndex: null,
+  // });  UNECCESSARY 
+
+  /**
+   * @summary 
+   * @param {number} rowId
+   * @param {number} colId 
+   * @returns {boolean}
+   */
+  const isCellSelected = (rowId, colId) =>{
+    const {rowIndex, colIndex} = props.clickCell
+    if(rowIndex === null && colIndex === null) return false
+    else if(rowId === rowIndex && colId === colIndex) return true
+    else return false
+
+  } 
 
   return (
     <div className="game">
@@ -18,8 +32,11 @@ function GameTable(props) {
                 <tr key={100000 + suRowIndex}>
                   {suRow.map((suValue, suColIndex) => {
                     
+                    const selectedStyle = (isCellSelected(suRowIndex,suColIndex)) ? {backgroundColor: "hsl(34, 26%, 89%)"} : null;
+                    // const selectedStyle ={backgroundColor: "red"}
                     return (
                       <td
+                        style ={selectedStyle}
                         key={suRowIndex * 10 + suColIndex}
                         onClick={() =>
                           props.onSelectCell(suRowIndex, suColIndex, suValue)
