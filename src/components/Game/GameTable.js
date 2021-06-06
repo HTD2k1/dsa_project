346e630d84ceb707f6879d7526dc0c1f9550d8a6
HTD2k1 <superdatlocpro@gmail.com>
2021-06-06 17:36:25 +0700

    Update GameTable.js

--- src/components/Game/GameTable.js
+++ src/components/Game/GameTable.js
@@ -1,12 +1,42 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import {createSetOfUnchangeableCells} from "../SudokuGenerator"
+import {isInputValid} from "../SudokuSolver"
 import "./GameTable.css";
 
 function GameTable(props) {
-  // const [clickCell, setClickCell] = React.useState({
-  //   rowIndex: null,
-  //   colIndex: null,
-  // });  UNECCESSARY 
+  //State
+  const [unchangeableSet, setUnchangeableSet] = useState(null);
+  const [invalidSet, setInvalidSet] = useState(null);
+  
+  useEffect(() =>{
+    const newSet = createSetOfUnchangeableCells(props.orgPuzzle)
+    setUnchangeableSet(newSet)
+    console.log("Inside useEffect_unchangeableSet", newSet)
+    
+  }, [props.orgPuzzle])
+
+  useEffect(() =>{
+    const tmpSet = createInValidSet(props.sudokuTableData);
+    console.log("Inside useEffect_invalidSet",tmpSet)
+    setInvalidSet(tmpSet)
+  },[props.sudokuTableData])
+
+
+
+  const createInValidSet =(board) =>{
+    const newSet = new Set()
+    for ( let i = 0; i <board.length; i++)
+      {
+        for (let j = 0; j <board[i].length;j++){ 
+          if (board[i][j] === 0) continue
+          if(isInputValid(board,board[i][j],i,j)) continue
+          else{
+              newSet.add(i+","+j);
+            }
+        }
+      }
+      return newSet
+  }
 
   /**
    * @summary 
@@ -22,6 +52,18 @@ function GameTable(props) {
 
   } 
 
+  
+
+  //Style
+  const styleContainer = {
+    selected: {backgroundColor: "hsl(34, 26%, 89%)"},
+    invalid:  {backgroundColor: "hsl(7, 63%, 67%)", color: "hsl(210, 88%, 56%)"},
+    unchangeable: {color: 'black'},
+    unchangeablyInvalid: {backgroundColor: "hsl(7, 63%, 67%)", color: 'black'}
+  }
+
+
+
   return (
     <div className="game">
       <table className="game__board">
@@ -32,15 +74,39 @@ function GameTable(props) {
                 <tr key={100000 + suRowIndex}>
                   {suRow.map((suValue, suColIndex) => {
                     
-                    const selectedStyle = (isCellSelected(suRowIndex,suColIndex)) ? {backgroundColor: "hsl(34, 26%, 89%)"} : null;
-                    // const selectedStyle ={backgroundColor: "red"}
+                    // Style condition
+                    const isInvalid = invalidSet &&  invalidSet.has(suRowIndex+","+suColIndex)
+
+                    //Styling input cell
+                    let normalCell = (isInvalid) ? 
+                    styleContainer.invalid : null;
+                     normalCell = (isCellSelected(suRowIndex,suColIndex)) ? styleContainer.selected : normalCell;
+                    //Styling unchangeable cell
+                    const unchangeableCell = (isInvalid) ? 
+                    styleContainer.unchangeablyInvalid : styleContainer.unchangeable;  
+
+                    if(unchangeableSet && !unchangeableSet.has(suRowIndex+","+suColIndex)) // checking if <td> is changeable
                     return (
+                      
+                      //Input <td>
                       <td
-                        style ={selectedStyle}
+                        style ={normalCell}
                         key={suRowIndex * 10 + suColIndex}
                         onClick={() =>
                           props.onSelectCell(suRowIndex, suColIndex, suValue)
                         }
+                        className={`game__cell game__cell--filled`}
+                      > 
+                      
+                        {suValue !== 0 ? suValue : null}
+                      </td>
+                    );
+                    
+                    //Unchangeable <td>
+                    else return(
+                      <td
+                        style ={unchangeableCell}
+                        key={suRowIndex * 10 + suColIndex}
                         className={`game__cell game__cell--filled`}
                       >
                         {suValue !== 0 ? suValue : null}
